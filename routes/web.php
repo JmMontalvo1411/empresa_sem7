b36d98d3a8190b1d7ccdc401ffed11c86c3d369a
Jm Montalvo <jm.montalvo.1411@gmail.com>
2024-07-14 11:03:25 -0500

    Avance

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\PersonaController;
+
+Route::view('/', 'welcome')->name('inicio');
+Route::view('/empresa', 'empresa')->name('empresa');
+Route::view('/contacto', 'contacto')->name('contacto');
+
+Route::resource('persona', PersonaController::class);
+Route::get('/personas', [PersonaController::class, 'index'])->name('personas.index');
+Route::get('/persona/{id}/edit', [PersonaController::class, 'edit'])->name('persona.edit');
+Route::put('/persona/{id}', [PersonaController::class, 'update'])->name('persona.update');
+Route::get('/personas/actualizar', [PersonaController::class, 'updateList'])->name('personas.updateList');
